81d88f9d67b9734c08c88c915d4c99461204d40d
Eric Loudon <loudon.eric@gmail.com>
2019-08-28 11:05:21 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,11 @@
 class Backer
 
-attr_accessor :name, :backed_projects
+attr_accessor :name, :backed_projects, :project
 
-def  initialize(name)
+def  initialize(name, project)
   @name = name
   @backed_projects = []
+  @backed_projects << project
 
 end
 
